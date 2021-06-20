c3da4003996c9999d2d754a7af12d87bf82153a7
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2021-06-20 16:03:33 +0530

    more routes and controller & Set up view engine

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controller/home_controller')
 router.get('/',homeController.home)
 router.get('/about',homeController.about)
+router.use('/users',require('./users'))
 
 console.log(  `Router in routes : ${5+5}`)
 module.exports = router;
